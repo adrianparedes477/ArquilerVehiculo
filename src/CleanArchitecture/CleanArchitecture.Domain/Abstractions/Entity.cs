1f4f3c4bde538d47616fe45e4dcbe2d47cfefd9b
Adrian Jesus Paredes <adrian.paredes@softtekprojects.com>
2024-01-15 21:55:21 -0300

    Domain Evebts y Notificaciones

--- src/CleanArchitecture/CleanArchitecture.Domain/Abstractions/Entity.cs
+++ src/CleanArchitecture/CleanArchitecture.Domain/Abstractions/Entity.cs
@@ -5,9 +5,26 @@ namespace CleanArchitecture.Domain.Abstractions;
 public abstract class Entity
 {
 
+    private readonly List<IDomainEvent> _domainEvents = new();
+
     protected Entity(Guid id)
     {
         Id = id;
     }
     public Guid Id {get; init;} // una vez q cree el objeto va a tener su valor identificador por siempre
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return _domainEvents.ToList();
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvents)
+    {
+        _domainEvents.Add(domainEvents);
+    }
 }
---
+++ src/CleanArchitecture/CleanArchitecture.Domain/Users/Events/UserCreateDomainEvent.cs
@@ -0,0 +1,9 @@
+
+using CleanArchitecture.Domain.Abstractions;
+
+namespace CleanArchitecture.Domain.Users.Events;
+
+public sealed record UserCreateDomainEvent(Guid UserId) :IDomainEvent
+{
+
+}
